ef23b56053eeb562e2183cd55f592c1c42efc41d
nasalidesu <55817845+nasalidesu@users.noreply.github.com>
2019-11-26 00:40:52 +0800

    Update jj.js

--- Assignment 2 javascript/jj.js
+++ Assignment 2 javascript/jj.js
@@ -8,6 +8,7 @@ function generateRandomNumber() {
 }
 
 function defineImgPath(result) {
+  //condition for dice and result
   switch (result) {
     case 1:
       path = "https://openclipart.org/download/282127/Die";
@@ -40,58 +41,48 @@ function defineImgPath(result) {
       document.getElementById("display1").innerHTML = " current postion is " +  currentPosition;
       break;
   }
+  // condition for ladder and snake
+  switch (currentPosition) {
+    case 8:
+      currentPosition = 14;
+      document.getElementById("display1").innerHTML = "LADDER! player advanced to pos 14, current postion is "+ currentPosition;
+        break;
+    case 19:
+      currentPosition = 24;
+      document.getElementById("display1").innerHTML = "LADDER! player advanced to pos 24, current postion is "+ currentPosition;
+        break;
+    case 15:
+      currentPosition = 5;
+      document.getElementById("display1").innerHTML = "SNAKE! player drop to pos 5, current postion is "+ currentPosition;
+      break;
+    case 23:
+      currentPosition = 16;
+      document.getElementById("display1").innerHTML = "SNAKE! player drop to pos 16, current postion is "+ currentPosition;
+        break;
+
+  }
+//condition to win the game >= 25
+  if (currentPosition >= 25) {
+    currentPosition=0;
+    document.getElementById("display1").innerHTML = "Player 1 WON the game !!";
 
 
-
-}
-
-function defineImgPath2(result2) {
-  switch (result2) {
-    case 1:
-      path = "https://openclipart.org/download/282127/Die";
-      currentPosition = currentPosition2 +result2;
-      document.getElementById("display2").innerHTML = " current postion is "+ currentPosition2;
-      break;
-    case 2:
-      path = "https://openclipart.org/download/282128/Die";
-      currentPosition = currentPosition2 +result2;
-      document.getElementById("display2").innerHTML = " current postion is "+ currentPosition2;
-      break;
-    case 3:
-      path = "https://openclipart.org/download/282129/Die";
-      currentPosition = currentPosition2 +result2;
-      document.getElementById("display2").innerHTML = " current postion is "+ currentPosition2;
-      break;
-    case 4:
-      path = "https://openclipart.org/download/282130/Die";
-      currentPosition = currentPosition2 +result2;
-      document.getElementById("display2").innerHTML = " current postion is "+ currentPosition2;
-      break;
-    case 5:
-      path = "https://openclipart.org/download/282131/Die";
-      currentPosition = currentPosition2 +result2;
-      document.getElementById("display2").innerHTML = " current postion is "+ currentPosition2;
-      break;
-    case 6:
-      path = "https://openclipart.org/download/282132/Die";
-      currentPosition = currentPosition2 +result2;
-      document.getElementById("display2").innerHTML = " current postion is " +  currentPosition2;
-      break;
   }
 
 
 
 }
 
+//player 2------------------------------------
+
+
+
+
 function changeDiceFace(result) {
   var dice = document.querySelector('#dice');
   dice.setAttribute("src", path + result + ".svg");
 }
 
-function changeDiceFace2(result2) {
-        var dice2 = document.querySelector('#dice2');
-        dice.setAttribute("src", path2 + result2 + ".svg");
-      }
 
 function rollDice() {
   var result = generateRandomNumber();
@@ -101,17 +92,16 @@ function rollDice() {
 
   return false;
 }
-
 function rollDice2() {
-        var result2 = generateRandomNumber();
-        defineImgPath2(result2);
-        changeDiceFace2(result2);
-      //function current positin 2 should be here
+var result = generateRandomNumber();
+changeDiceFace(result);
+    document.getElementById("display2").innerHTML = "Player 2 WON the game !!"+result ;
 
-        return false;
-      }
+  return false;
+}
 
 
+//testing purposes-------
 function positionLA(currentPosition){
   switch (currentPosition) {
     case 8:
